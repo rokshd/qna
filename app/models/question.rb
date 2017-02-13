2a47863dd51c6730eba9f5ebe0e2a60f145d28df
rokshd <rokshd@gmail.com>
2017-02-13 23:27:03 +0300

    added tests for answer model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
+  has_many :answer
   validates :title, :body, presence: true
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,5 +14,4 @@ RSpec.describe Question, type: :model do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-
 end
