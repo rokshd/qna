60955674a981639c8a18867006b38a31a8ab21fb
rokshd <rokshd@gmail.com>
2017-02-17 22:38:51 +0300

    create action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,17 +4,33 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
   def show
   end
+
   def new
     @question = Question.new
   end
+
   def edit
+  end
+
+  def create
+    @question = Question.new(questions_params)
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
   end
 
   private
 
   def load_question
     @question = Question.find(params[:id])
+  end
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
   end
 end
