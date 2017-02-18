33ced037f41a3aa4b9267fd89b2b1e4e90124e83
rokshd <rokshd@gmail.com>
2017-02-18 21:09:54 +0300

    fix comments

--- spec/controllers/answer_controller_spec.rb
+++ spec/controllers/answer_controller_spec.rb
@@ -38,8 +38,7 @@ RSpec.describe AnswerController, type: :controller do
     context 'with invalid attributes' do
       it 'does not save the answer' do
         expect { post :create, params: { question_id: question, answer:
-          attributes_for(:invalid_answer) } }.to_not change(question.answers,
-            :count)
+          attributes_for(:invalid_answer) } }.to_not change(Answer, :count)
       end
       it 're-renders new view' do
         post :create, params: { question_id: question,
