c96651fa5cd446b4ed11b1a400b8f9f6d0254ea6
rokshd <rokshd@gmail.com>
2017-02-18 16:58:20 +0300

    clean up

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,18 +1,7 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @questions = Question.all
-  end
-
-  def show
-  end
 
   def new
     @question = Question.new
-  end
-
-  def edit
   end
 
   def create
@@ -24,24 +13,7 @@ class QuestionsController < ApplicationController
     end
   end
 
-  def update
-    if @question.update(question_params)
-      redirect_to @question
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @question.destroy
-    redirect_to question_path
-  end
-
   private
-
-  def load_question
-    @question = Question.find(params[:id])
-  end
 
   def question_params
     params.require(:question).permit(:title, :body)
