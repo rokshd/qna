f4ce739c971153b60378f9e74246195fc69be9c6
rokshd <rokshd@gmail.com>
2017-03-02 21:18:44 +0300

    fix user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,28 +5,20 @@ RSpec.describe User do
   it { should validate_presence_of :password }
 
   describe "author_of?" do
-    let(:man_in_black) { create(:user) }
     let(:user) { create(:user) }
+    let(:man_in_black) { create(:user) }
     let(:question) { create(:question, user: man_in_black) }
-    let(:answer) { create(:answer, question: question, user: man_in_black)}
+    let(:answer) { create(:answer, question: question, user: man_in_black) }
 
     context 'user_id and object_id are the same' do
       it "compares user_id and question_id" do
-        expect(man_in_black.author_of?(question)).to eq true
-      end
-
-      it "compares user_id and answer_id" do
-        expect(man_in_black.author_of?(answer)).to eq true
+        expect(man_in_black).to be_author_of(question)
       end
     end
 
     context 'user_id and object_id are different' do
       it "compares user_id and question_id" do
-        expect(user.author_of?(question)).to eq false
-      end
-
-      it "compares user_id and answer_id" do
-        expect(user.author_of?(answer)).to eq false
+        expect(user).to_not be_author_of(question)
       end
     end
   end
